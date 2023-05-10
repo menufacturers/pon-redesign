80ca16195818e1d9ae1f30153011e60ffed3ed6e
Grace <122253132+wygkim22@users.noreply.github.com>
2023-05-09 18:47:08 -0700

    added restaurant images

--- src/pages/Restaurants.js
+++ src/pages/Restaurants.js
@@ -6,12 +6,19 @@ export default function Restaurants() {
             <h1>Hello this is the restaurants page.</h1>
             <ul>
                 <li>Search</li>
+
                 <li>Filter + sort</li>
                 <li>Restaurant cards: distance, hours, cuisine</li>
                 <section id="cards">
                 <div class="container">
-                    <div class="row align-items-center">
-                   
+                    <div class="col-lg-4">
+                        <img src="../assets/img/restaurants/alem.png" alt="alem restaurant" class="img-fluid" />
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="../assets/img/restaurants/bananasgrill.png" alt="bgrill restaurant" class="img-fluid" />
+                    </div>
+                    <div class="col-lg-4">
+                        <img src="../assets/img/restaurants/banhmi.png" alt="banhmi restaurant" class="img-fluid" />
                     </div>
                 </div>
             </section>

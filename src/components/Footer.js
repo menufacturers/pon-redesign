cd0a1ff11dab7670cfec2b76c84ea0742b55d34e
Jennifer Do <jdo17@uw.edu>
2023-05-13 04:50:41 -0700

    Add social media icons to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,10 @@ export default function Footer() {
                     <div class="col-md-4">
                     </div>
                     <div class="col-md-4">
+                        <a class="socials-icon" href="https://www.facebook.com/plateofnations"><i class="fa-brands fa-square-facebook"></i></a>
+                        <a class="socials-icon" href="https://www.linkedin.com/company/plate-of-nations/"><i class="fa-brands fa-linkedin"></i></a>
+                        <a class="socials-icon" href="https://www.instagram.com/plateofnations/"><i class="fa-brands fa-square-instagram"></i></a>
+                        <a class="socials-icon" href="https://www.tiktok.com/@plateofnations23"><i class="fa-brands fa-tiktok"></i></a>
                     </div>
                 </div>
             </div>

53388d01dd3365552fc9258298d365be2ebb190e
Jack Weinstein <37884621+JRWeinstein@users.noreply.github.com>
2023-05-18 20:40:07 -0700

    added alem restaurant page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Passport from './pages/Passport';
 import Press from './pages/Press';
 import About from './pages/About';
 import Portal from './pages/Portal';
-// import Alem from './pages/subpages/Alem';
+import Alem from './pages/subpages/Alem';
 
 function App() {
   return (
@@ -17,12 +17,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="restaurants" element={<Restaurants />} />
-        {/* <Route path="restaurants/alem" element={<Alem />} /> */}
+        <Route path="alem" element={<Alem />} />
         <Route path="digital-passport" element={<Portal />} />
         <Route path="passport" element={<Passport />} />
         <Route path="press" element={<Press />} />
         <Route path="about" element={<About />} />
-        <Route path="digital-passport" element={<Portal />} />
       </Routes>
       <Footer />
     </div>
